ba912f75932a5943cc46e246678599311f29efa2
Jan Haderka <jan.haderka@magnolia-cms.com>
2018-02-07 15:15:48 +0100

    'persons' english usage

--- plugins/tokens/tokens.py
+++ plugins/tokens/tokens.py
@@ -63,15 +63,17 @@ class Tokens(BotPlugin):
             be = 'are'
             hold = 'hold'
 
-            if len(results) == 0:
-                persons = 'and no one'
-            else:
-                persons = str(results).strip('[]')
+        if len(results) == 0:
+            persons = 'no one'
+        else:
+            persons = str(results).strip('[]')
 
         if token_type == 'p':
             result = '**'+persons+'** '+hold+' token'
             # print("res:"+result)
         else:
+            if len(results) == 0:
+                persons = 'and no one'
             # print("number of results:" + str(len(results)))
             result = '**' + persons + '** ' + be + ' queued for one'
             # print(result)
