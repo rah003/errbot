0873378f94ac54f2a8d3db3d91b3d6f7210a4b3e
Jan Haderka <jan.haderka@magnolia-cms.com>
2018-02-16 14:20:32 +0100

    new plugin to notify about absence

---
+++ plugins/ooofice/ooofice.py
@@ -0,0 +1,23 @@
+import gspread
+import json
+import os
+from datetime import date
+
+from errbot import BotPlugin, botcmd
+from oauth2client.service_account import ServiceAccountCredentials
+
+
+class OOOfice(BotPlugin):
+    """
+    This is a very basic plugin to tell ppl I'm on hols when they mention my name.
+    """
+
+    def callback_message(self, mess):
+        endday = date(2018, 2, 25)
+        today = date.today()
+        if mess.body.find('@yan') != -1 and today < endday:
+            self.send(
+                mess.frm,
+                "Please be informed that Yan is on holidays until Feb 25th",
+            )
+
